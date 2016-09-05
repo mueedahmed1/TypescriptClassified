bb70c717fff2ceae0b6561ca1dc9d8837040236f
mueedahmed1 <goodiuck_2010@yahoo.com>
2016-09-05 21:19:42 +0500

    Edit feature in progress

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var DataArr = [];
 var btn = document.getElementById("btn");
 var listt = document.getElementById("list");
-var HoldData = JSON.parse(localStorage.getItem("data"));
+var HoldData = JSON.parse(localStorage.getItem("data")) || [];
 console.log(HoldData);
 if (btn) {
     btn.addEventListener("click", function () {
@@ -10,22 +10,44 @@ if (btn) {
 }
 function showData() {
     HoldData.forEach(function (item, index) {
-        var gen = "<ul id='ind" + index + "'>  \n                  <li>  " + item.product + "  </li> \n                  <li> " + item.contact + " </li> \n                  <li>  " + item.price + " </li>\n                  <li>  <button id='id" + index + "'>Delete</button></li>\n                  </ul>";
+        var gen = "<ul id='ind" + index + "'>  \n                  <li>  " + item.product + " </li> \n                  <li> " + item.contact + " </li> \n                  <li>  " + item.price + " </li>\n                  <li>  <button id='id" + index + "'>Delete</button><button id=\"edit" + index + "\">Edit</button></li>\n                  </ul>";
         listt.innerHTML += gen;
         // console.log(document.getElementById(`id${index}`));
-        document.getElementById("id" + index).addEventListener("click", function (index) {
-            console.log(index);
-        });
-        //console.log(document.getElementById(`id${index}`).parentNode.parentNode);
     });
+    HoldData.forEach(function (item, index) {
+        document.getElementById("id" + index).addEventListener("click", function (e) {
+            //  console.log(e);
+            //  console.log(index);
+            deletefunc(index);
+        });
+        document.getElementById("edit" + index).addEventListener("click", function (e) {
+            //  console.log(e);
+            //  console.log(index);
+            Edit(index);
+        });
+    });
+    //console.log(document.getElementById(`id${index}`).parentNode.parentNode);
 }
+;
 function deletefunc(indexx) {
-    var nodee = document.getElementById("id" + indexx).parentNode;
-    console.log(nodee);
+    // let nodee = document.getElementById("id" + indexx).parentNode; 
+    var chk = JSON.parse(localStorage.getItem("data"));
+    // console.log(chk);
+    chk.splice(indexx, 1);
+    localStorage.setItem("data", JSON.stringify(chk));
+    // console.log(indexx);  
+    // document.getElementById(`ind${indexx}`).innerHTML = ""  
+    listt.removeChild(document.getElementById("ind" + indexx));
+}
+;
+function Edit(edtindx) {
+    console.log("Edit work fine");
+    console.log(edtindx);
 }
 if (listt) {
     showData();
 }
+;
 function AddintoArr() {
     var productName = document.getElementById('input1').value;
     var contact = document.getElementById('input2').value;
@@ -38,3 +60,4 @@ function AddintoArr() {
     localStorage.setItem("data", JSON.stringify(DataArr));
     window.location.href = 'secondpage.html';
 }
+;
--- app.ts
+++ app.ts
@@ -1,7 +1,7 @@
 let DataArr: {}[] = []
 let btn = document.getElementById("btn");
 let listt = document.getElementById("list");
-let HoldData = JSON.parse(localStorage.getItem("data"));
+let HoldData = JSON.parse(localStorage.getItem("data")) || [];
 console.log(HoldData);
 
 
@@ -17,29 +17,53 @@ if (btn) {
 function showData() {
     HoldData.forEach(function (item, index) {
         var gen = `<ul id='ind${index}'>  
-                  <li>  ${item.product}  </li> 
+                  <li>  ${item.product} </li> 
                   <li> ${item.contact} </li> 
                   <li>  ${item.price} </li>
-                  <li>  <button id='id${index}'>Delete</button></li>
-                  </ul>`;   
+                  <li>  <button id='id${index}'>Delete</button><button id="edit${index}">Edit</button></li>
+                  </ul>`;
 
-                  listt.innerHTML += gen;
-// console.log(document.getElementById(`id${index}`));
+        listt.innerHTML += gen;
+        // console.log(document.getElementById(`id${index}`));
 
-document.getElementById(`id${index}`).addEventListener("click",function(index){
- console.log(index);
-})
-                 //console.log(document.getElementById(`id${index}`).parentNode.parentNode);
-    }
-
-    );
-}
+    });
 
 
+    HoldData.forEach(function (item, index) {
+        document.getElementById(`id${index}`).addEventListener("click", function (e) {
+            //  console.log(e);
 
-function deletefunc(indexx){
-let nodee = document.getElementById("id" + indexx).parentNode;
-console.log(nodee);
+            //  console.log(index);
+            deletefunc(index);
+
+        });
+
+        document.getElementById(`edit${index}`).addEventListener("click", function (e) {
+            //  console.log(e);
+
+            //  console.log(index);
+            Edit(index);
+
+        });
+    });
+    //console.log(document.getElementById(`id${index}`).parentNode.parentNode);
+};
+
+function deletefunc(indexx) {
+    // let nodee = document.getElementById("id" + indexx).parentNode; 
+    let chk = JSON.parse(localStorage.getItem("data"));
+    // console.log(chk);
+    chk.splice(indexx, 1);
+    localStorage.setItem("data", JSON.stringify(chk));
+    // console.log(indexx);  
+    // document.getElementById(`ind${indexx}`).innerHTML = ""  
+    listt.removeChild(document.getElementById(`ind${indexx}`));
+};
+
+
+function Edit(edtindx) {
+    console.log("Edit work fine");
+    console.log(edtindx);
 }
 
 
@@ -48,7 +72,7 @@ if (listt) {
     showData();
     // console.log("under");
     //  list.appendChild();
-}
+};
 
 function AddintoArr() {
     let productName = (<HTMLInputElement>document.getElementById('input1')).value;
@@ -63,4 +87,4 @@ function AddintoArr() {
 
     localStorage.setItem("data", JSON.stringify(DataArr));
     window.location.href = 'secondpage.html';
-}
+};
