37802e4b14f0f5ad79f734eaf34b3422c607a7c8
mueedahmed1 <goodiuck_2010@yahoo.com>
2016-09-10 20:48:51 +0500

    validation feature in progress

--- Signup.js
+++ Signup.js
@@ -17,21 +17,63 @@
 //     let password = (<HTMLInputElement>document.getElementById('pass')).value;
 //     console.log(user + "" + email + "" + password);
 // }
-function one() {
-    console.log("one");
-}
-;
-function two() {
-    console.log("two");
-}
-function submitt() {
-    one();
-    two();
-}
 var chk = document.getElementsByName("choosename")[0];
 var chk1 = document.getElementsByName("email")[0];
+var chk2 = document.getElementsByName("password")[0];
 console.log(chk1);
 console.log(chk);
-// chk.addEventListener("focusout", function(){
-//     submitt();
-// }) 
+console.log(chk2);
+chk.addEventListener("focusout", function () {
+    checkEmptyInput();
+});
+function checkEmptyInput() {
+    if (chk.value.length == 0) {
+        document.getElementById("err").removeAttribute("style");
+    }
+    else {
+        allLetter();
+    }
+}
+;
+function allLetter() {
+    var letters = /^[A-Za-z]+$/;
+    if (chk.value.match(letters)) {
+        document.getElementById("correct").removeAttribute("style");
+    }
+    else {
+        document.getElementById("charonly").removeAttribute("style");
+    }
+}
+;
+chk.addEventListener("focusin", function () {
+    document.getElementById("err").setAttribute("style", "display:none");
+    document.getElementById("correct").setAttribute("style", "display:none");
+    document.getElementById("charonly").setAttribute("style", "display:none");
+});
+chk1.addEventListener("focusout", function () {
+    checkEmptyEmail();
+});
+function checkEmptyEmail() {
+    if (chk1.value.length == 0) {
+        document.getElementById("err1").removeAttribute("style");
+    }
+    else {
+        ValidateEmail();
+    }
+}
+;
+function ValidateEmail() {
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{3,3})+$/;
+    if (chk1.value.match(mailformat)) {
+        document.getElementById("correct1").removeAttribute("style");
+    }
+    else {
+        document.getElementById("charonly1").removeAttribute("style");
+    }
+}
+;
+chk1.addEventListener("focusin", function () {
+    document.getElementById("err1").setAttribute("style", "display:none");
+    document.getElementById("correct1").setAttribute("style", "display:none");
+    document.getElementById("charonly1").setAttribute("style", "display:none");
+});
--- Signup.ts
+++ Signup.ts
@@ -20,23 +20,76 @@
 
 // }
 
-function one(){
-console.log("one");
-};
+let chk = document.getElementsByName("choosename")[0];
+let chk1 = document.getElementsByName("email")[0];
+let chk2 = document.getElementsByName("password")[0];
 
-function two(){
-    console.log("two");
-}
-
-function submitt(){
-    one();
-    two();
-}
-
-var chk = document.getElementsByName("choosename")[0];
-var chk1 = document.getElementsByName("email")[0];
 console.log(chk1);
 console.log(chk);
-// chk.addEventListener("focusout", function(){
-//     submitt();
-// })
+console.log(chk2);
+
+chk.addEventListener("focusout", function () {
+    checkEmptyInput();
+
+});
+
+function checkEmptyInput() {
+    if ((<HTMLInputElement>chk).value.length == 0) {
+        document.getElementById("err").removeAttribute("style");
+    }
+    else {
+        allLetter();
+    }
+};
+
+function allLetter() {
+    var letters = /^[A-Za-z]+$/;
+    if ((<HTMLInputElement>chk).value.match(letters)) {
+        document.getElementById("correct").removeAttribute("style");
+    }
+    else {
+        document.getElementById("charonly").removeAttribute("style");
+    }
+};
+
+chk.addEventListener("focusin", function () {
+    document.getElementById("err").setAttribute("style", "display:none");
+    document.getElementById("correct").setAttribute("style", "display:none");
+    document.getElementById("charonly").setAttribute("style", "display:none");
+
+});
+
+
+chk1.addEventListener("focusout", function(){
+checkEmptyEmail();
+});
+
+function checkEmptyEmail() {
+    if ((<HTMLInputElement>chk1).value.length == 0) {
+        document.getElementById("err1").removeAttribute("style");
+    }
+    else {
+         ValidateEmail();
+    }
+};
+
+
+function ValidateEmail() {
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{3,3})+$/;
+    if ((<HTMLInputElement>chk1).value.match(mailformat)) {
+        document.getElementById("correct1").removeAttribute("style");
+        
+    }
+    else {
+
+        document.getElementById("charonly1").removeAttribute("style");
+
+    }
+};
+
+chk1.addEventListener("focusin", function () {
+    document.getElementById("err1").setAttribute("style", "display:none");
+    document.getElementById("correct1").setAttribute("style", "display:none");
+    document.getElementById("charonly1").setAttribute("style", "display:none");
+
+});
