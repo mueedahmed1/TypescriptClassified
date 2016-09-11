c486d09d29a69634674644e905c0146a75702fa2
mueedahmed1 <goodiuck_2010@yahoo.com>
2016-09-11 19:40:55 +0500

    push changes

--- Signup.js
+++ Signup.js
@@ -1,22 +1,3 @@
-// function allLetter(inputtxt)  
-//   {  
-//    var letters = /^[A-Za-z]+$/;  
-//    if(inputtxt.value.match(letters))  
-//      {  
-//       return true;  
-//      }  
-//    else  
-//      {  
-//      alert("message");  
-//      return false;  
-//      }  
-//   }  
-// function required(){
-//     let user = (<HTMLInputElement>document.getElementById("user")).value;
-//     let email = (<HTMLInputElement>document.getElementById('email')).value;
-//     let password = (<HTMLInputElement>document.getElementById('pass')).value;
-//     console.log(user + "" + email + "" + password);
-// }
 var chk = document.getElementsByName("choosename")[0];
 var chk1 = document.getElementsByName("email")[0];
 var chk2 = document.getElementsByName("password")[0];
@@ -77,3 +58,73 @@ chk1.addEventListener("focusin", function () {
     document.getElementById("correct1").setAttribute("style", "display:none");
     document.getElementById("charonly1").setAttribute("style", "display:none");
 });
+chk2.addEventListener("focusout", function () {
+    checkEmptyPassword();
+});
+function checkEmptyPassword() {
+    if (chk2.value.length == 0) {
+        document.getElementById("err2").removeAttribute("style");
+    }
+    else {
+        CheckPassword();
+    }
+}
+;
+function CheckPassword() {
+    var passw = /^[A-Za-z]\w{7,14}$/;
+    if (chk2.value.match(passw)) {
+        document.getElementById("correct2").removeAttribute("style");
+    }
+    else {
+        document.getElementById("charonly2").removeAttribute("style");
+    }
+}
+;
+chk2.addEventListener("focusin", function () {
+    document.getElementById("err2").setAttribute("style", "display:none");
+    document.getElementById("correct2").setAttribute("style", "display:none");
+    document.getElementById("charonly2").setAttribute("style", "display:none");
+});
+function checkData() {
+    if (chk.value.length == 0) {
+        alert("username required");
+    }
+    if (chk1.value.length == 0) {
+        alert("Email Address required");
+    }
+    if (chk2.value.length == 0) {
+        alert("Password required");
+    }
+    else {
+        submitData();
+    }
+}
+;
+function submitData() {
+    var letters = /^[A-Za-z]+$/;
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{3,3})+$/;
+    var passw = /^[A-Za-z]\w{7,14}$/;
+    var cmp = chk.value.match(letters);
+    var cmp1 = chk1.value.match(mailformat);
+    var cmp2 = chk2.value.match(passw);
+    if (chk.value && cmp) {
+        var setuser = chk.value;
+    }
+    if (chk1.value && cmp1) {
+        var setEmail = chk1.value;
+    }
+    if (chk2.value && cmp2) {
+        var setPass = chk2.value;
+    }
+    if ((chk.value && cmp) && (chk1.value && cmp1) && (chk2.value && cmp2)) {
+        console.log(setuser + " " + setEmail + " " + setPass);
+        alert("you have Successfully Register");
+        chk.value = "";
+        chk1.value = "";
+        chk2.value = "";
+        window.location.href = "Signin.html";
+    }
+    else {
+        alert("data is not submitted because something went wrong");
+    }
+}
--- Signup.ts
+++ Signup.ts
@@ -1,24 +1,3 @@
-// function allLetter(inputtxt)  
-//   {  
-//    var letters = /^[A-Za-z]+$/;  
-//    if(inputtxt.value.match(letters))  
-//      {  
-//       return true;  
-//      }  
-//    else  
-//      {  
-//      alert("message");  
-//      return false;  
-//      }  
-//   }  
-
-// function required(){
-//     let user = (<HTMLInputElement>document.getElementById("user")).value;
-//     let email = (<HTMLInputElement>document.getElementById('email')).value;
-//     let password = (<HTMLInputElement>document.getElementById('pass')).value;
-//     console.log(user + "" + email + "" + password);
-
-// }
 
 let chk = document.getElementsByName("choosename")[0];
 let chk1 = document.getElementsByName("email")[0];
@@ -36,6 +15,7 @@ chk.addEventListener("focusout", function () {
 function checkEmptyInput() {
     if ((<HTMLInputElement>chk).value.length == 0) {
         document.getElementById("err").removeAttribute("style");
+        // (<HTMLInputElement>chk).focus();
     }
     else {
         allLetter();
@@ -49,6 +29,7 @@ function allLetter() {
     }
     else {
         document.getElementById("charonly").removeAttribute("style");
+        // (<HTMLInputElement>chk).focus();
     }
 };
 
@@ -60,16 +41,17 @@ chk.addEventListener("focusin", function () {
 });
 
 
-chk1.addEventListener("focusout", function(){
-checkEmptyEmail();
+chk1.addEventListener("focusout", function () {
+    checkEmptyEmail();
 });
 
 function checkEmptyEmail() {
     if ((<HTMLInputElement>chk1).value.length == 0) {
         document.getElementById("err1").removeAttribute("style");
+        // (<HTMLInputElement>chk1).focus();
     }
     else {
-         ValidateEmail();
+        ValidateEmail();
     }
 };
 
@@ -78,12 +60,12 @@ function ValidateEmail() {
     var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{3,3})+$/;
     if ((<HTMLInputElement>chk1).value.match(mailformat)) {
         document.getElementById("correct1").removeAttribute("style");
-        
+
     }
     else {
 
         document.getElementById("charonly1").removeAttribute("style");
-
+        //  (<HTMLInputElement>chk1).focus();
     }
 };
 
@@ -93,3 +75,105 @@ chk1.addEventListener("focusin", function () {
     document.getElementById("charonly1").setAttribute("style", "display:none");
 
 });
+
+chk2.addEventListener("focusout", function () {
+    checkEmptyPassword();
+});
+
+
+function checkEmptyPassword() {
+    if ((<HTMLInputElement>chk2).value.length == 0) {
+        document.getElementById("err2").removeAttribute("style");
+        // (<HTMLInputElement>chk2).focus();
+    }
+    else {
+        CheckPassword();
+    }
+};
+
+function CheckPassword() {
+    var passw = /^[A-Za-z]\w{7,14}$/;
+    if ((<HTMLInputElement>chk2).value.match(passw)) {
+        document.getElementById("correct2").removeAttribute("style");
+    }
+    else {
+        document.getElementById("charonly2").removeAttribute("style");
+        // (<HTMLInputElement>chk2).focus();
+    }
+};
+
+chk2.addEventListener("focusin", function () {
+    document.getElementById("err2").setAttribute("style", "display:none");
+    document.getElementById("correct2").setAttribute("style", "display:none");
+    document.getElementById("charonly2").setAttribute("style", "display:none");
+
+});
+
+function checkData() {
+    if ((<HTMLInputElement>chk).value.length == 0) {
+        alert("username required");
+        // (<HTMLInputElement>chk).focus();
+    }
+    if ((<HTMLInputElement>chk1).value.length == 0) {
+        alert("Email Address required");
+        // (<HTMLInputElement>chk1).focus();
+    }
+
+    if ((<HTMLInputElement>chk2).value.length == 0) {
+        alert("Password required");
+        // (<HTMLInputElement>chk2).focus();
+    }
+
+    // if ((<HTMLInputElement>chk2).value.length) {
+    //     if ((<HTMLInputElement>chk2).value.length < 7) {
+    //         // alert("Password length too short");
+    //         (<HTMLInputElement>chk2).focus();
+    //     }
+
+    //     if ((<HTMLInputElement>chk2).value.length > 16) {
+    //         // alert("Password length too long");
+    //         (<HTMLInputElement>chk2).focus();
+    //     }
+    // }
+
+    else {
+        submitData();
+    }
+
+};
+
+function submitData() {
+    var letters = /^[A-Za-z]+$/;
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{3,3})+$/;
+    var passw = /^[A-Za-z]\w{7,14}$/;
+    var cmp = (<HTMLInputElement>chk).value.match(letters);
+    var cmp1 = (<HTMLInputElement>chk1).value.match(mailformat);
+    var cmp2 = (<HTMLInputElement>chk2).value.match(passw);
+
+
+    if ((<HTMLInputElement>chk).value && cmp) {
+        var setuser = (<HTMLInputElement>chk).value;
+    }
+
+    if ((<HTMLInputElement>chk1).value && cmp1) {
+        var setEmail = (<HTMLInputElement>chk1).value;
+    }
+
+    if ((<HTMLInputElement>chk2).value && cmp2) {
+        var setPass = (<HTMLInputElement>chk2).value;
+    }
+    if (((<HTMLInputElement>chk).value && cmp) && ((<HTMLInputElement>chk1).value && cmp1) && ((<HTMLInputElement>chk2).value && cmp2)) {
+        console.log(setuser + " " + setEmail + " " + setPass);
+        alert("you have Successfully Register");
+    (<HTMLInputElement>chk).value = "";
+    (<HTMLInputElement>chk1).value = "";
+    (<HTMLInputElement>chk2).value = "";
+
+        window.location.href = "Signin.html";
+    }
+
+    else {
+        alert("data is not submitted because something went wrong");
+    }
+    
+}
